526e8cb8b5062d16b3f5c2846dee6eca445320c9
ArtisticZhao <hit.yuhao.zhao@gmail.com>
2024-03-31 23:44:40 +0800

    High DPI compatibility

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
 import sys
 from PyQt5 import QtWidgets
 from Qt.MainWindow import MainWindow
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtGui import QFont
+from PyQt5.QtCore import Qt
+
 
 if __name__ == "__main__":
+    # 1. Enabling Global Scaling
+    QApplication.setAttribute(Qt.AA_EnableHighDpiScaling, True) 
+
+    # 2. Non-Integer Scaling Adaptation
+    QApplication.setHighDpiScaleFactorRoundingPolicy(Qt.HighDpiScaleFactorRoundingPolicy.PassThrough)
+
+    # 4.
     app = QtWidgets.QApplication(sys.argv)
+
+    # 3. Font Rendering Hinting Adaptation
+    font = QFont()
+    font.setStyleStrategy(QFont.PreferAntialias)
+    font.setHintingPreference(QFont.HintingPreference.PreferFullHinting)
+    QApplication.setFont(font)
+
     myapp = MainWindow()
     myapp.show()
     sys.exit(app.exec_())
